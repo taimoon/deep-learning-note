f08c8281d697425ea6d71525d252ea85d9bc44ac
Leong Teng Man <41246170+taimoon@users.noreply.github.com>
2023-05-12 17:35:09 +0800

    change

--- dir_init.py
+++ dir_init.py
@@ -3,7 +3,7 @@ from os.path import abspath
 from os import getcwd,makedirs,listdir
 from subprocess import run
 
-for dir in ('data','model','results','results/history'):
+for dir in ('data','results','results/history'):
     makedirs(dir,exist_ok=True)
     
 dir = ".env"
